943e90639cbe3a5cd75787dd0dfee548e5c74b86
CamiloAlvarez <andresalvarez09092004@gmail.com>
2023-10-02 20:58:32 -0500

    .

--- server/server1.c
+++ server/server1.c
@@ -4,7 +4,8 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include <time.h> 
+#include <time.h>
+#include <fcntl.h>
 
 
 #define PORT_A 8080
@@ -16,11 +17,6 @@ typedef struct {
     int vertical;
 } Data;
 
-typedef struct {
-    char type[100];  // 'init' o cualquier otro tipo de mensaje
-    Data data;
-} Message;
-
 void init_ball_velocity(Data *ball_vel, int right) {
     ball_vel->horizontal = rand() % 3 + 2;  // Genera un número entre 2 y 4
     ball_vel->vertical = rand() % 2 + 1;    // Genera un número entre 1 y 2
@@ -30,37 +26,27 @@ void init_ball_velocity(Data *ball_vel, int right) {
     }
 }
 
-void send_ball_velocity_init(int socket_a, int socket_b, Message ball_vel) {
-    char jsonString[200];
-    snprintf(jsonString, sizeof(jsonString), "{\"type\":\"%s\",\"data\":[%d,%d]}", ball_vel.type, ball_vel.data.horizontal, ball_vel.data.vertical);
+void send_ball_velocity_init(int socket_a, int socket_b, char type[], Data ball_vel_a) {
+    char encodedData_a[100];
+    char encodedData_b[100];
+    Data ball_vel_b = ball_vel_a;
+    ball_vel_b.horizontal = -ball_vel_b.horizontal;
 
+    snprintf(encodedData_a, sizeof(encodedData_a), "%s;%d;%d", type, ball_vel_a.horizontal, ball_vel_a.vertical);
+    snprintf(encodedData_b, sizeof(encodedData_b), "%s;%d;%d", type, ball_vel_b.horizontal, ball_vel_b.vertical);
     // Envía la información de ball_vel a ambos sockets
-    send(socket_a, jsonString, strlen(jsonString), 0);
-    send(socket_b, jsonString, strlen(jsonString), 0);
+    send(socket_a, encodedData_a, strlen(encodedData_a), 0);
+    send(socket_b, encodedData_b, strlen(encodedData_b), 0);
 }
 
-// void extract_type_value(const char *json_data, char *type_value) {
-//     cJSON *root = cJSON_Parse(json_data);  // Parsear el JSON
-//     if (root) {
-//         cJSON *type_item = cJSON_GetObjectItemCaseSensitive(root, "type");
-//         if (cJSON_IsString(type_item)) {
-//             strcpy(type_value, type_item->valuestring);
-//         } else {
-//             strcpy(type_value, "");  // No se encontró el campo 'type'
-//         }
-//         cJSON_Delete(root);  // Liberar memoria
-//     } else {
-//         strcpy(type_value, "");  // Error al parsear el JSON
-//     }
-// }
 
 int main(int argc, char const* argv[]) {
     int server_fd_a, server_fd_b, new_socket_a, new_socket_b;
     struct sockaddr_in address_a, address_b;
     int opt = 1;
     int addrlen = sizeof(address_a);
-	Message buffer_received_a[1024] = { 0 };
-    Message buffer_received_b[1024] = { 0 };
+	char buffer_received_a[1024] = { 0 };
+    char buffer_received_b[1024] = { 0 };
     char* hello = "Hello from server";
 
     if ((server_fd_a = socket(AF_INET, SOCK_STREAM, 0)) < 0 ||
@@ -107,14 +93,17 @@ int main(int argc, char const* argv[]) {
 		exit(EXIT_FAILURE);
 	}
 
+    // Establecer el socket en modo no bloqueante
+    int flags_a = fcntl(new_socket_a, F_GETFL, 0);
+    fcntl(new_socket_a, F_SETFL, flags_a | O_NONBLOCK);
+    int flags_b = fcntl(new_socket_b, F_GETFL, 0);
+    fcntl(new_socket_b, F_SETFL, flags_b | O_NONBLOCK);
+
     srand(time(NULL));
     Data ball_velocity;
     // init_ball_velocity(&ball_velocity, (rand()%2));
     init_ball_velocity(&ball_velocity, 0);
-    Message initMessage;
-    strcpy(initMessage.type, "ball_start");
-    initMessage.data = ball_velocity;
-    send_ball_velocity_init(new_socket_a, new_socket_b, initMessage);
+    send_ball_velocity_init(new_socket_a, new_socket_b, "ball_start", ball_velocity);
     fflush(stdout);
 
 	while (1) {
@@ -127,46 +116,31 @@ int main(int argc, char const* argv[]) {
             close(new_socket_b);
             break;
         }
-        char type_value[100];
-        // extract_type_value(buffer_received_a->type, type_value);
-        // printf("Comparación: %s\n", type_value);
-        // printf("Comparación: %s\n", type_value);
-        printf("Comparación: %d\n", strcmp(buffer_received_a->type, "point_made"));
-        printf("Comparación: %s\n", buffer_received_a->type);
-        printf("Comparación: %d\n", strcmp(buffer_received_b->type, "point_made"));
-        printf("Comparación: %s\n", buffer_received_b->type);
-
-        fflush(stdout);
         if (bytes_read_a > 0) {
-            if(strcmp(buffer_received_a->type, "point_made") == 0){
+            printf(format_string, buffer_received_a);
+            if(strcmp(buffer_received_a, "point_made") == 0){
                 init_ball_velocity(&ball_velocity, (rand()%2));
-                Message initMessage;
-                strcpy(initMessage.type, "ball_start");
-                initMessage.data = ball_velocity;
-                printf(format_string, "point_made");
-                send_ball_velocity_init(new_socket_a, new_socket_b, initMessage);
+                send_ball_velocity_init(new_socket_a, new_socket_b, "ball_start", ball_velocity);
                 continue;
             }
             // Enviar datos del cliente A al cliente B
             printf(format_string, "A -> B");
+            fflush(stdout);
             send(new_socket_b, buffer_received_a, bytes_read_a, 0);
         }
 
         if (bytes_read_b > 0) {
-            if(strcmp(buffer_received_b->type, "point_made") == 0){
+            printf(format_string, buffer_received_b);
+            if(strcmp(buffer_received_b, "point_made") == 0){
                 init_ball_velocity(&ball_velocity, (rand()%2));
-                Message initMessage;
-                strcpy(initMessage.type, "ball_start");
-                initMessage.data = ball_velocity;
-                printf(format_string, "point_made");
-                send_ball_velocity_init(new_socket_a, new_socket_b, initMessage);
+                send_ball_velocity_init(new_socket_a, new_socket_b, "ball_start", ball_velocity);
                 continue;
             }
             // Enviar datos del cliente B al cliente A
             printf(format_string, "B -> A");
+            fflush(stdout);
             send(new_socket_a, buffer_received_b, bytes_read_b, 0);
         }
-        fflush(stdout);
     }
 
 
