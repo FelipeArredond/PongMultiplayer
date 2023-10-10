3c975e6922f5f60f97cfb7e8ec106743e44ee619
CamiloAlvarez <andresalvarez09092004@gmail.com>
2023-10-10 18:54:14 -0500

    thread modify and client too

--- client/pong.py
+++ client/pong.py
@@ -4,6 +4,7 @@ from pygame.locals import *
 import threading
 import atexit
 import re
+import time
 
 pygame.init()
 fps = pygame.time.Clock()
@@ -13,6 +14,7 @@ WHITE = (255,255,255)
 RED = (255,0,0)
 GREEN = (0,255,0)
 BLACK = (0,0,0)
+
 
 #globals
 WIDTH = 600
@@ -28,8 +30,14 @@ local_paddle_pos = [HALF_PAD_WIDTH - 1, HEIGHT/2]
 enemy_paddle_pos = [WIDTH +1 - HALF_PAD_WIDTH,HEIGHT/2]
 local_paddle_vel = 0
 enemy_paddle_vel = 0
+player_score = -1
+opponent_score = 0
+point_was_made = False
 KEYDOWN_WAS_ENTER = False
 THREADS_ARE_RUNNING = True
+FONT = pygame.font.SysFont("Consolas", int(WIDTH/20))
+player_name = ''
+opponent_name = ''
 
 #canvas declaration
 window = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
@@ -37,8 +45,8 @@ pygame.display.set_caption('Pong Game')
 
 server_response_event = threading.Event()
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = ('44.201.112.24', 8080)
-# server_address = ('localhost', 8080)
+# server_address = ('34.238.40.118', 8080)
+server_address = ('localhost', 8080)
 
 def close_threads():
     global THREADS_ARE_RUNNING
@@ -54,22 +62,50 @@ def ball_init(ball_vel_init):
 
 
 def receive_data_from_server():
-    global enemy_paddle_pos, THREADS_ARE_RUNNING
+    global enemy_paddle_pos, THREADS_ARE_RUNNING, player_score, point_was_made,player_name, opponent_name, FONT
     while THREADS_ARE_RUNNING:
         response = client_socket.recv(24)
-
+        if not response:
+            print('Server closed connection')
+            close_threads()
+            break
         dataDecoded = response.decode()
         print(f'Data recieved: {dataDecoded}')
-        type, x, y = dataDecoded.split(';')
-        if type == 'ball_start':
-            print(f'X: {x}')
-            print(f'Y: {y}')
-            ball_init([float(x), float(y)])  # horizontal, vertical
-            server_response_event.set()
-        elif type == 'paddle_move':
-            y = re.sub(r'[a-zA-Z]', '', y)
-            print(f'Enemy Y: {y}')
-            enemy_paddle_pos = [float(x), float(y)]  # x, y
+        dataDecoded = dataDecoded.split('|')
+        for data in dataDecoded:
+            print(f'Data: {data}')
+            try:    
+                type, x, y = data.split(';')
+                if type == 'bs': #ball_start
+                    if not point_was_made:
+                        player_score += 1
+                    point_was_made = False
+                    print(f'X: {x}')
+                    print(f'Y: {y}')
+                    ball_init([float(x), float(y)])  # horizontal, vertical
+                    server_response_event.set()
+                elif type == 'pmv': #paddle_move
+                    y = re.sub(r'[a-zA-Z]', '', y)
+                    print(f'Enemy Y: {y}')
+                    enemy_paddle_pos = [float(x), float(y)]  # x, y
+                elif type == 'fg': # finish game
+                    print('Game finished')
+                    close_threads()
+                    winner_name = player_name
+                    loser_name = opponent_name
+                    if(player_score < opponent_score):
+                        winner_name = opponent_name
+                        loser_name = player_name
+                    print(f'Winner: {winner_name}')
+                    print(f'Loser: {loser_name}')
+                    window.fill(BLACK)
+                    window.blit(FONT.render(f'El ganador es {winner_name}.', True, "green"), (WIDTH/2-300, HEIGHT/2-50))
+                    window.blit(FONT.render(f'El perdedor es {loser_name}.', True, "red"), (WIDTH/2-300, HEIGHT/2))
+                    pygame.display.update()
+                    time.sleep(10)
+                    break
+            except Exception as e:
+                print(f'[ERROR] -> {e}')
 
 #keydown handler
 def keydown(event):
@@ -90,11 +126,17 @@ def keyup(event):
 
 
 def game():
-    global ball_pos, ball_vel, KEYDOWN_WAS_ENTER, THREADS_ARE_RUNNING
+    global ball_pos, ball_vel, KEYDOWN_WAS_ENTER, THREADS_ARE_RUNNING, player_score, opponent_score, point_was_made, player_name, opponent_name
 
     receive_data_thread = threading.Thread(target=receive_data_from_server)
 
     client_socket.connect(server_address)
+    player_name = input("Introduce un nombre de usuario de hasta 24 caracteres: ")
+    if len(player_name) > 24:
+        player_name = player_name[:24]
+    dataEncoded = f'{player_name}'.encode()
+    client_socket.send(dataEncoded)
+    opponent_name = client_socket.recv(24).decode()
     receive_data_thread.start()
     server_response_event.wait()
     server_response_event.clear()
@@ -109,6 +151,11 @@ def game():
             local_paddle_pos[1] += local_paddle_vel
         elif local_paddle_pos[1] == HEIGHT - HALF_PAD_HEIGHT and local_paddle_vel < 0:
             local_paddle_pos[1] += local_paddle_vel
+        else:
+            if KEYDOWN_WAS_ENTER:
+                dataEncoded = f'pmv;{enemy_paddle_pos[0]};{local_paddle_pos[1]}|'.encode()
+                client_socket.send(dataEncoded)
+            KEYDOWN_WAS_ENTER = False
  
     
         #update ball
@@ -119,7 +166,11 @@ def game():
         pygame.draw.circle(window, WHITE, ball_pos, 20, 0)
         pygame.draw.polygon(window, WHITE, [[local_paddle_pos[0] - HALF_PAD_WIDTH, local_paddle_pos[1] - HALF_PAD_HEIGHT], [local_paddle_pos[0] - HALF_PAD_WIDTH, local_paddle_pos[1] + HALF_PAD_HEIGHT], [local_paddle_pos[0] + HALF_PAD_WIDTH, local_paddle_pos[1] + HALF_PAD_HEIGHT], [local_paddle_pos[0] + HALF_PAD_WIDTH, local_paddle_pos[1] - HALF_PAD_HEIGHT]], 0)
         pygame.draw.polygon(window, WHITE, [[enemy_paddle_pos[0] - HALF_PAD_WIDTH, enemy_paddle_pos[1] - HALF_PAD_HEIGHT], [enemy_paddle_pos[0] - HALF_PAD_WIDTH, enemy_paddle_pos[1] + HALF_PAD_HEIGHT], [enemy_paddle_pos[0] + HALF_PAD_WIDTH, enemy_paddle_pos[1] + HALF_PAD_HEIGHT], [enemy_paddle_pos[0] + HALF_PAD_WIDTH, enemy_paddle_pos[1] - HALF_PAD_HEIGHT]], 0)
-
+        player_score_text = FONT.render(str(player_score), True, "white")
+        opponent_score_text = FONT.render(str(opponent_score), True, "white")
+        window.blit(player_score_text, (WIDTH/2-50, 50))
+        window.blit(opponent_score_text, (WIDTH/2+50, 50))
+        
         #ball collision check on top and bottom walls
         if int(ball_pos[1]) <= BALL_RADIUS:
             ball_vel[1] = - ball_vel[1]
@@ -133,7 +184,10 @@ def game():
             ball_vel[0] *= 1.1
             ball_vel[1] *= 1.1
         elif int(ball_pos[0]) <= BALL_RADIUS + PAD_WIDTH:
-            dataEncoded = b'point_made'
+            dataEncoded = b'pmd'
+            opponent_score += 1
+            point_was_made = True
+            ball_pos = [WIDTH/2,HEIGHT/2]
             client_socket.send(dataEncoded)
             server_response_event.wait()
             server_response_event.clear()
@@ -144,10 +198,12 @@ def game():
             ball_vel[0] *= 1.1
             ball_vel[1] *= 1.1
         elif int(ball_pos[0]) >= WIDTH + 1 - BALL_RADIUS - PAD_WIDTH:
+            ball_vel[0] *= 1.1
+            ball_vel[1] *= 1.1
             ball_vel[0] = -ball_vel[0]
 
         if KEYDOWN_WAS_ENTER:
-            dataEncoded = f'paddle_move;{enemy_paddle_pos[0]};{local_paddle_pos[1]}'.encode()
+            dataEncoded = f'pmv;{enemy_paddle_pos[0]};{local_paddle_pos[1]}|'.encode()
             client_socket.send(dataEncoded)
 
         for event in pygame.event.get():
--- server/threads.c
+++ server/threads.c
@@ -8,7 +8,6 @@
 #include <arpa/inet.h>
 #include <fcntl.h>
 
-#define SERVER_PORT 8080
 #define MAXLINE 4096
 #define format_string "%s\n"
 #define POINT_LIMIT 5
@@ -40,6 +39,36 @@ void sendBallVelocity(int socket1, int socket2, char type[], Data ballData);
 
 int main(int argc, char const *argv[])
 {
+
+
+    if (argc != 3) {
+        // Verificar si se proporcionaron los argumentos requeridos
+        printf("Uso: %s <PORT> <Log File>\n", argv[0]);
+        return 1; // Salir con un código de error
+    } 
+
+    int SERVER_PORT = atoi(argv[1]);
+    char *logFile = argv[2];
+
+
+    if (SERVER_PORT <= 0 || SERVER_PORT > 65535) {
+        printf("Puerto inválido\n");
+        return 1; // Salir con un código de error
+    }
+
+    // Abrir el archivo de registro en modo escritura (si no existe, se creará)
+    FILE *log = fopen(logFile, "w");
+
+    if (log == NULL) {
+        perror("Error al abrir el archivo de registro");
+        return 1; // Salir con un código de error
+    }
+
+    // Si se desea ver los outputs en consola se la pasa cualquier argumento adicional
+    if( argc == 3){
+        dup2(fileno(log), STDOUT_FILENO);
+    }
+    
     int listenfd, connfd, n;
     struct sockaddr_in serverAddr;
 
@@ -101,11 +130,13 @@ int main(int argc, char const *argv[])
         else
         {
             printf("Waiting for another player...\n");
+            fflush(stdout);
             game.player1 = player;
             game.isWaiting = 1;
         }
     }
-
+    // Cerrar el archivo de registro cuando hayas terminado de usarlo
+    fclose(log);
     return 0;
 }
 
@@ -114,16 +145,19 @@ void *handle_game(void *args) {
     Player player1 = game.player1;
     Player player2 = game.player2;
 
-	char bufferPlayer1[24] = { 0 };
-    char bufferPlayer2[24] = { 0 };
+	char bufferPlayer1[100] = { 0 };
+    char bufferPlayer2[100] = { 0 };
     // Leer nombres de usuario.
     int bytesRead1 = read(player1.socket, bufferPlayer1, sizeof(bufferPlayer1));
     int bytesRead2 = read(player2.socket, bufferPlayer2, sizeof(bufferPlayer2));
     strcpy(player1.name, bufferPlayer1);
     strcpy(player2.name, bufferPlayer2);
-    
+    printf("Player 1: %s | VS | Player 2: %s\n", player1.name, player2.name);
+    fflush(stdout);
+
     send(player1.socket, player2.name, strlen(player2.name), 0);
     send(player2.socket, player1.name, strlen(player1.name), 0);
+    printf("Names were sent to players\n");
     sleep(2);
     // Establecer el socket en modo no bloqueante
     // int flags_a = fcntl(player1.socket, F_GETFL, 0);
@@ -139,16 +173,15 @@ void *handle_game(void *args) {
     initBallVelocity(&ballData, 0);
     sendBallVelocity(player1.socket, player2.socket, "bs", ballData);
     printf("Game started!\n");
-    printf("Player 1: %s | VS | Player 2: %s", player1.name, player2.name);
     fflush(stdout);
 
 	while (game.scorePlayer1 < POINT_LIMIT && game.scorePlayer2 < POINT_LIMIT) {
         // Leer datos del cliente A
-        // memset(bufferPlayer1, 0, sizeof(bufferPlayer1));
+        memset(bufferPlayer1, 0, sizeof(bufferPlayer1));
         bytesRead1 = read(player1.socket, bufferPlayer1, sizeof(bufferPlayer1));
 
+        memset(bufferPlayer2, 0, sizeof(bufferPlayer2));
         // Leer datos del cliente B
-        // memset(bufferPlayer2, 0, sizeof(bufferPlayer2));
         bytesRead2 = read(player2.socket, bufferPlayer2, sizeof(bufferPlayer2));
 
         if(bytesRead1 == 0 || bytesRead2 == 0){
@@ -156,40 +189,41 @@ void *handle_game(void *args) {
         }
         
         if (bytesRead1 > 0) {
-            printf(format_string, bufferPlayer1);
+            printf("Player 1 Buffer: %s\n", bufferPlayer1);
             if(strstr(bufferPlayer1, "pmd")!= NULL){
                 game.scorePlayer2++;
+                memset(bufferPlayer1, 0, sizeof(bufferPlayer1));
                 initBallVelocity(&ballData, (rand()%2));
                 sendBallVelocity(player1.socket, player2.socket, "bs", ballData);
-                memset(bufferPlayer1, 0, sizeof(bufferPlayer1));
                 continue;
             }
             // Enviar datos del cliente A al cliente B
-            printf(format_string, "A -> B");
-            fflush(stdout);
             send(player2.socket, bufferPlayer1, bytesRead1, 0);
+            printf("Player1 %s send buffer to Player2 %s\n", player1.name, player2.name);
+            fflush(stdout);
         }
 
         if (bytesRead2 > 0) {
-            //printf(format_string, bufferPlayer2);
+            printf("Player 2 Buffer: %s\n", bufferPlayer2);
             if(strstr(bufferPlayer2, "pmd") != NULL){
                 game.scorePlayer1++;
+                memset(bufferPlayer2, 0, sizeof(bufferPlayer2));
                 initBallVelocity(&ballData, (rand()%2));
                 sendBallVelocity(player1.socket, player2.socket, "bs", ballData);
-                memset(bufferPlayer1, 0, sizeof(bufferPlayer2));
                 continue;
             }
             // Enviar datos del cliente B al cliente A
-            printf(format_string, "B -> A");
-            fflush(stdout);
             send(player1.socket, bufferPlayer2, bytesRead2, 0);
+            printf("Player2 %s send buffer to Player1 %s\n", player2.name, player1.name);
+            fflush(stdout);
         }
     }
 
     //Enviar datos de fin de juego
     send(player1.socket, "fg;;", strlen("fg;;"), 0);
     send(player2.socket, "fg;;", strlen("fg;;"), 0);
-    
+    printf("Game finished. FeedBack was sent to clients.\n");
+    fflush(stdout);
     shutdown(player1.socket, SHUT_RDWR);
     shutdown(player2.socket, SHUT_RDWR);
     return NULL;
@@ -210,10 +244,10 @@ void sendBallVelocity(int socket1, int socket2, char type[], Data ballData) {
 
     Data ballData2 = ballData;
     ballData2.horizontal = -ballData2.horizontal;
-
     snprintf(encodedData1, sizeof(encodedData1), "%s;%d;%d|", type, ballData.horizontal, ballData.vertical);
     snprintf(encodedData2, sizeof(encodedData2), "%s;%d;%d|", type, ballData2.horizontal, ballData2.vertical);
     // Envía la información de ball_vel a ambos sockets
     send(socket1, encodedData1, strlen(encodedData1), 0);
     send(socket2, encodedData2, strlen(encodedData2), 0);
+    printf("Ball Velocity sent to each player: %d, %d\n", ballData.horizontal, ballData.vertical);
 }
